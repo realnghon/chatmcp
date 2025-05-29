30af6399f10f51eb43329dc9d52589fac9afd3a6
daodao97 <daodao97@foxmail.com>
2025-05-29 12:39:36 +0800

    up

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     namespace = "run.daodao.chatmcp"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    // ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
