9b0977288f976ba241293f678aeaefbbd64ffb28
daodao97 <daodao97@foxmail.com>
2025-06-02 09:51:10 +0800

    upgrade flutter

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,24 @@ subprojects {
             jvmToolchain(17)
         }
     }
+    
+    // 强制所有子项目使用相同的 Kotlin 编译选项
+    afterEvaluate {
+        if (extensions.findByName("android") != null) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+        
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
